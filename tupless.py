b5ba92739c453eda510d39284571ce798afdc3a0
kirey1412 <kireykayana14@gmail.com>
2024-08-08 19:11:43 +0700

    Add files via upload

---
+++ tupless.py
@@ -0,0 +1,20 @@
+city=("sydney","venice","paris","tokyo")
+print(city[2])
+
+city1,city2, city3, city4=city
+print(city4)
+
+moods=("happy", "sad", "anxious", "angry")
+print(moods[0:3])
+
+colors=("orange","red","blue","yellow")
+fruits=("orange","apples","berry","banana")
+print(colors + fruits)
+
+countries=("japan","uk","india","maldives","france")
+print(countries[3:5])
+
+#slicingtuples
+yourname=('M','I','C','H','E','L','L','E')
+print(yourname[2:6])
+
